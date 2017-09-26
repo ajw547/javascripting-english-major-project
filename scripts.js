79e54206731a4e2b7a5e4beed16cee01511c575d
Amelia Wood <Amelia@Amelias-MacBook-Pro.local>
2017-09-25 22:56:47 -0400

    ch. 4

--- scripts.js
+++ scripts.js
@@ -8,3 +8,11 @@ if (i%2!==0){
 $("#response").append("<br />"+i+ " is odd.");
 }
 }
+
+let tipCalculator;
+tipCalculator = function(total, tipRate){
+  let tipAmount;
+  tipAmount = tipRate * total;
+  $("#response").html("Your tip is $" + tipAmount);
+};
+tipCalculator(50.00, 0.2);
