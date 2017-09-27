eb5f6f64d7d052f2cf6c097bb761dc2e47565239
Amelia Wood <Amelia@Amelias-MacBook-Pro.local>
2017-09-27 14:49:56 -0400

    ch. 4 exercises

--- scripts.js
+++ scripts.js
@@ -12,7 +12,12 @@ $("#response").append("<br />"+i+ " is odd.");
 let tipCalculator;
 tipCalculator = function(total, tipRate){
   let tipAmount;
-  tipAmount = tipRate * total;
+  if(tipRate===20){
+    tipAmount=tipRate*total/100;
+  }
+  if(tipRate===0.2){
+    tipAmount=tipRate*total;
+  }
   $("#response").html("Your tip is $" + tipAmount);
 };
-tipCalculator(50.00, 0.2);
+tipCalculator(50.00, 20);
