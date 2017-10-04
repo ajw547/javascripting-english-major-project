b734e63130708fb3a6f3018c2775366029333afd
Amelia Wood <Amelia@Amelias-MacBook-Pro.local>
2017-10-03 21:05:24 -0400

    Ch. 5 exercise 3

--- scripts.js
+++ scripts.js
@@ -1,23 +1,7 @@
 let userInput;
-userInput=prompt("Type a number.");
-for(let i=1; i<=userInput; i=i+1){
-if (i%2===0){
-$("#response").append("<br />"+i+ " is even.");
+userInput= prompt("Type a number");
+if(userInput.isInteger()){
+  $("#response").html("<br/>" + "The number is an integer.");
+} else {
+  $("#response").append("<br/>" + "The number is not an integer.");
 }
-if (i%2!==0){
-$("#response").append("<br />"+i+ " is odd.");
-}
-}
-
-let tipCalculator;
-tipCalculator = function(total, tipRate){
-  let tipAmount;
-  if(tipRate===20){
-    tipAmount=tipRate*total/100;
-  }
-  if(tipRate===0.2){
-    tipAmount=tipRate*total;
-  }
-  $("#response").html("Your tip is $" + tipAmount);
-};
-tipCalculator(50.00, 20);
