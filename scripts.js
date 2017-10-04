5a7d52abf913d7d7faba66fdd490a2733bab9c27
Amelia Wood <Amelia@Amelias-MacBook-Pro.local>
2017-10-03 21:09:08 -0400

    ch. 5 exercise 1

--- scripts.js
+++ scripts.js
@@ -1,7 +1,4 @@
-let userInput;
-userInput= prompt("Type a number");
-if(userInput.isInteger()){
-  $("#response").html("<br/>" + "The number is an integer.");
-} else {
-  $("#response").append("<br/>" + "The number is not an integer.");
-}
+let a;
+a=["a","b","c","d"];
+a.pop();
+$("#response").html(a);
