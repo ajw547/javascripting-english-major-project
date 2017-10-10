f3613242fd364594e6a9e2e6b599167ce2e9a29a
Amelia Wood <Amelia@Amelias-MacBook-Pro.local>
2017-10-10 18:58:07 -0400

    ch. 6 exercise 2

--- scripts.js
+++ scripts.js
@@ -1,5 +1,14 @@
-let numbers = [1,2,3];
-let doubledNumbers = numbers.map(function(x){
-  return x * 2;
-});
-$("#response").html(doubledNumbers);
+let leonardo, donatello, raphael, michelangelo, turtles, weapons;
+leonardo = {name: "Leonardo", color: "blue", weapon: "katana"};
+donatello = {name: "Donatello", color: "purple", weapon: "bo"};
+raphael = {name: "Raphael", color: "red", weapon: "sai"};
+michelangelo = {name: "Michelangelo", color: "blue", weapon: "nunchaku"};
+turtles = [leonardo, donatello, raphael, michelangelo];
+weapons = turtles.map(function(turtle){
+  return turtle.weapon;
+}).sort();
+let removeSai;
+removeSai = weapons.filter(function(weapon){
+  return weapon.includes("o")+ weapon.includes("k");
+}).join (", ");
+$("#response").html(removeSai + ", and sai");
