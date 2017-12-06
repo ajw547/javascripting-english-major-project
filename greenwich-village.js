1138acf78c5ea018c38fbf6e50ee496cc0e90e0d
Amelia Wood <Amelia@Amelias-MacBook-Pro.local>
2017-12-06 16:20:18 -0500

    json edit

--- greenwich-village.js
+++ greenwich-village.js
@@ -9,21 +9,21 @@ L.tileLayer("https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x
     tileLayer.addTo(theVillageMap);
 theVillageMap.setView([40.73084, -73.99745], 14.5);
 
-let md;
-md = window.markdownit({html: true}).use(window.markdownitFootnote);
-$.ajax({
-  url: "https://ajw547.github.io/javascripting-english-major-project/greenwich-village.md",
-  success: function(markdown){
-    let html;
-    html = md.render(markdown);
-    $("#content").html(html);
-  }
-});
-$("#tabs-nav a[href='#greenwich-village']").tab("show");
-
-let charlesStreet, charlesStreetLine, charlesStreetMarker;
-charlesStreet=[[40.73532, -74.00043],[40.73383, -74.01006]];
-charlesStreetLine=L.polyline(charlesStreet, {color: "#BE3626"}
-).addTo(theVillageMap);
-charlesStreetMarker=L.marker([40.73479, -74.00401]);
-charlesStreetMarker.addTo(theVillageMap);
+let projectFeatures;
+$.getJSON("https://ajw547.github.io/javascripting-english-major-project/greenwich-village.geo.json",function(data){
+let projectLayer;
+  projectFeatures=data.features.map(function(feature){
+    return{
+      name:feature.properties.name,
+      html:feature.properties.html,
+      tab:feature.properties.tab,
+      wikipedia:feature.properties.wikipedia,
+      latLng:L.latLng(feature.geometry.coordinates[1], feature.geometry.coordinates[0])
+    };
+   });
+   projectLayer = L.featureGroup(projectFeatures.map(function(feature){
+     return L.marker(feature.latLng);
+     })
+   );
+  projectLayer.addTo(theVillageMap);
+ });
