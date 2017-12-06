d93e956e963a22d3a69effcd457eb3de71d4d4e2
Amelia Wood <Amelia@Amelias-MacBook-Pro.local>
2017-12-06 17:00:21 -0500

    edits

--- greenwich-village.js
+++ greenwich-village.js
@@ -11,19 +11,13 @@ theVillageMap.setView([40.73084, -73.99745], 14.5);
 
 let projectFeatures;
 $.getJSON("https://ajw547.github.io/javascripting-english-major-project/greenwich-village.geo.json",function(data){
-
-  projectFeatures=data.features.map(function(feature){
-    return{
-      name:feature.properties.name,
-      html:feature.properties.html,
-      tab:feature.properties.tab,
-      wikipedia:feature.properties.wikipedia,
-      latLng:L.latLng(feature.geometry.coordinates[1], feature.geometry.coordinates[0])
-    };
-   });
-   let projectLayer = L.featureGroup(projectFeatures.map(function(feature){
-     return L.marker(feature.latLng);
-     })
-   );
-  projectLayer.addTo(theVillageMap);
- });
+projectFeatures= data.features.map(function(feature){
+  return{
+    name:feature.properties.name,
+    html:feature.properties.html,
+    tab:feature.properties.tab,
+    wikipedia:feature.properties.wikipedia,
+    latLng:L.latLng(feature.geometry.coordinates[1],feature.geometry.coordinates[0])
+  };
+});
+});
