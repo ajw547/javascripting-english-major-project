83cdc806c9c3425faf6ffc16be66df7d61a9e160
Amelia Wood <Amelia@Amelias-MacBook-Pro.local>
2017-12-07 09:58:58 -0500

    -

---
+++ charles-street.md
@@ -0,0 +1,3 @@
+## Charles Street
+
+The first mention of Charles Street is in the first chapter of The Unpossessed. In this chapter, Margaret is walking home from the grocer's, musing as she does so. "This year on Charles Street," remarks the narrator, "last year on Tenth; surely these were stations along some route?"
--- greenwich-village.js
+++ greenwich-village.js
@@ -1,23 +1,21 @@
-let theVillageMap, tileLayer;
-theVillageMap = L.map("the-village-map");
+let md;
+md = window.markdownit({html: true}).use(window.markdownitFootnote);
+$.ajax({
+  url: "https://ajw547.github.io/javascripting-english-major-project/greenwich-village.md",
+  success: function(markdown){
+    let html;
+    html = md.render(markdown);
+    $("#content").html(html);
+  }
+});
+
+let map, tileLayer;
+map = L.map("the-village-map");
 tileLayer =
 L.tileLayer("https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png", {
       attribution: "&copy; <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> &copy; <a href='http://carto.com/attribution'>CARTO</a>",
       subdomains: "abcd",
       maxZoom: 18
     });
-    tileLayer.addTo(theVillageMap);
-theVillageMap.setView([40.73084, -73.99745], 14.5);
-
-let projectFeatures;
-$.getJSON("https://ajw547.github.io/javascripting-english-major-project/greenwich-village.geo.json",function(data){
-projectFeatures= data.features.map(function(feature){
-  return{
-    name:feature.properties.name,
-    html:feature.properties.html,
-    tab:feature.properties.tab,
-    wikipedia:feature.properties.wikipedia,
-    latLng:L.latLng(feature.geometry.coordinates[1],feature.geometry.coordinates[0])
-  };
-});
-});
+    tileLayer.addTo(map);
+map.setView([40.73084, -73.99745], 14.5);
